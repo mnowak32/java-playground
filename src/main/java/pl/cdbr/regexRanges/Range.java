fd34c65f6e47d79197380087c353f3becb844ffa
Michał Nowak <michal.nowak6@orange.com>
2016-11-24 15:39:13 +0100

    Generating regexes for numeric ranges

---
+++ src/main/java/pl/cdbr/regexRanges/Range.java
@@ -0,0 +1,123 @@
+package pl.cdbr.regexRanges;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Range {
+    //potrzebne do sortowania
+    private final static Comparator<Range> comparator = (r1, r2) -> Integer.compare(r1.from, r2.from);
+    
+    final int from, to;
+    
+    Range(int f, int t) {
+        from = f;
+        to = t;
+    }
+    
+    List<Range> splitToPower(int p) {
+        int pow = (int) Math.pow(10, p);
+        int splitFrom = (int) (from / pow) * pow;
+        int splitTo = splitFrom + pow - 1;
+        List<Range> spl = new ArrayList<>();
+        while (splitFrom <= to) {
+            Range r = new Range(from > splitFrom ? from : splitFrom, to < splitTo ? to : splitTo);
+            spl.add(r);
+            splitFrom += pow;
+            splitTo += pow;
+        }
+        return spl;
+    }
+    
+    boolean isFullInPower(int p) {
+        int pow = (int) Math.pow(10, p);
+        int lowest = (int) (from / pow) * pow;
+        int highest = lowest + pow - 1;
+        return (from == lowest && to == highest);
+    }
+    
+    public String toString() {
+        return String.format("Range[%d -> %d]", from, to);
+    }
+    
+    Range joinRanges(List<Range> rs) {
+        rs.sort(comparator);
+        return new Range(rs.get(0).from, rs.get(rs.size() - 1).to);
+    }
+    
+    List<Range> split() {
+        int maxPower = (int) Math.log10(to);
+        return split(maxPower);
+    }
+    
+    List<Range> split(int pow) {
+        //jeżeli zeszliśmy do jedności (10^0), to już nie ma co dzielić
+        if (pow == 0) {
+            return Arrays.asList(this);
+        }
+        
+        
+        //podzielenie na pod-zakresy wg zadanej potęgi
+        final Map<Boolean, List<Range>> partitioned = splitToPower(pow).stream()
+        //podzielenie na full (true) i nie-full (false) - wynik ląduje w Map<Boolean, List<Range>>
+            .collect(Collectors.partitioningBy(r -> r.isFullInPower(pow)));
+        
+        //nie-full (czyli początek i/lub koniec) lecą do dalszego przetwarzania
+        final List<Range> notFull = partitioned.get(Boolean.FALSE);
+        
+        //każdy nie-full jest rekurencyjnie splitowany poziom niżej (o jedną potęgę 10 mniej)
+        final List<Range> spl = notFull.stream()
+            .flatMap( r -> r.split(pow - 1).stream())
+            .collect(Collectors.toList());
+        
+        //dołączamy do listy zakresy full
+        List<Range> toJoin = partitioned.get(Boolean.TRUE);
+        if (toJoin.size() > 0) {
+            spl.add(joinRanges(toJoin));
+        }
+        //posortowane rosnąco
+        spl.sort(comparator);
+        
+        return spl;
+    }
+    
+    public String toSimpleRegex() {
+        final String fromStr = Integer.toString(from);
+        final String toStr = Integer.toString(to);
+        if (fromStr.length() != toStr.length()) {
+            //nie jest dobrze
+            return "";
+        }
+        final StringBuilder rx = new StringBuilder();
+        for (int i = 0; i < fromStr.length(); i++) {
+            final char f = fromStr.charAt(i);
+            final char t = toStr.charAt(i);
+            if (f == t) {
+                rx.append(f);
+            } else {
+                //"[f-t]"
+                rx.append('[').append(f).append('-').append(t).append(']');
+            }
+        }
+        return rx.toString();
+    }
+    
+    public String toRegex() {
+        List<Range> rs = split();
+//        System.out.println(rs);
+        String rx = rs.stream()
+            .map(Range::toSimpleRegex)
+            .collect(Collectors.joining("|"));
+        return rx;
+    }
+    
+    public static void main(String[] args) {
+        String rx = new Range (2, 6910).toRegex();
+        System.out.println(rx);
+    }
+}
+
+
